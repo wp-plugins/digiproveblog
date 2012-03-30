b9af3622fe9219a73e36c783b92d8864b090ac28
DigiproveDevelopment <DigiproveDevelopment@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-30 19:01:10 +0000

    Release 1.16
    git-svn-id: https://plugins.svn.wordpress.org/digiproveblog/tags/1.16@525465 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/copyright_proof_http_functions.php
@@ -0,0 +1,125 @@
+<?php
+
+// THIS VERSION USES TRY/CATCH BLOCKS AND NEEDS PHP5 OR LATER
+
+/* this function based on that from akismet.php by Matt Mullenweg.  */
+function dprv_http_post($request, $host, $path, $service, $ip=null) 
+{
+	//global $dprv_port, $dprv_ssl;
+	global $dprv_port;
+	$log = new Logging();  
+	$request = "xml_string=" . urlencode($request);
+	$http_request  = "POST " . $path . $service . " HTTP/1.1\r\n";
+	$http_request .= "Host: $host\r\n";
+	$http_request .= "Content-Type: application/x-www-form-urlencoded; charset=" . get_option('blog_charset') . "\r\n";
+	$http_request .= "Content-Length: " . strlen($request) . "\r\n";
+	$http_request .= "Connection: close\r\n\r\n";
+	$http_request .= $request;  
+
+	// use a specific IP if provided
+	if ( $ip && long2ip(ip2long($ip)) )
+	{
+		$http_host = $ip;
+	}
+	else
+	{
+		//$http_host = akismet_get_host($host);  //TODO: implement this akismet resilience code
+		$http_host = $host;
+	}
+	//$log->lwrite("http_post of " . $http_request);
+
+	$response = '';                 
+	if (DPRV_SSL == "Yes")
+	{
+		$t = stream_get_transports();
+		for ($i=0; $i<count($t); $i++)
+		{
+			if (stripos($t[$i], "ssl") !== false)
+			{
+				$http_host = "ssl://" . $http_host;
+				break;
+			}
+		}
+		if (strpos($http_host, "ssl://") === false)
+		{
+			$dprv_port = 80;
+		} 
+	}
+	$log->lwrite("http_host " . $http_host);
+	try																	// try/catch block not supported in php4
+	{
+		$errno = -1;
+		$errstr = "Unknown";
+		if( false != ( $fs = @fsockopen($http_host, $dprv_port, $errno, $errstr, 10) ) ) 
+		{                 
+			//$log->lwrite("socket open, errno = " . $errno);
+			if ($errno == 0)
+			{
+				fwrite($fs, $http_request);
+				stream_set_timeout($fs, 50);
+				$get_count = 0;
+				$err_level = error_reporting();							// Save current error reporting level
+				while ( !feof($fs) )
+				{
+					error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);	// Suppress Warning errors just for this due to intermittent bug (in IIS?)
+					$temp = fgets($fs);
+					//error_reporting(E_ALL & ~E_NOTICE);				// Reset error reporting back to default
+					error_reporting($err_level);						// Reset error reporting back to previous value
+					$info = stream_get_meta_data($fs);
+					if ($info['timed_out'])
+					{
+						$log->lwrite("timed out waiting for response");
+						return "Error: connection timed out, server may be offline";
+					}
+					else
+					{
+						$response .= $temp;
+						$get_count = $get_count + 1;
+					}
+				}
+				$log->lwrite("finished getting, about to close socket");
+				fclose($fs);
+				//TODO: check that response is complete (ends with </string>)
+				$response = htmlspecialchars_decode($response, ENT_QUOTES);
+				$log->lwrite("response length=" . strlen($response));
+				if (strlen($response) == 0)
+				{
+					$log->lwrite("Empty response from server");
+					return "Error: " . $http_host . " returned empty response, server may be offline";
+				}
+			}
+			else
+			{
+				$log->lwrite("Socket may be open, but error = " . $errno . "/" . $errstr);
+				return "Error: Could not open socket to " . $http_host . ".  Error = " . $errno . "/" . $errstr;
+			}
+		}
+		else
+		{
+			if ($errno ==0)
+			{
+				$log->lwrite("Could not initialise socket");
+				return "Error: Could not initialise socket to " . $http_host . ", server may be offline";
+			}
+			$log->lwrite("Could not open socket, error = " . $errno . "/" . $errstr);
+			return "Error: Could not open socket to " . $http_host . ", server may be offline.  Error = " . $errno . "/" . $errstr;
+		}
+		//$log->lwrite("Got response ok: " . $response);
+		// TODO: Trap HTTP errors such as 403 here (happens if you try to connect to live server w/o ssl)
+		if (substr($response,0,4) == "HTTP")			// error starts like this: HTTP/1.1 404 Not Found
+		{	
+			$pos = strpos($response, " ");
+			if (substr($response, $pos+1, 3) != "200" && strpos(strtolower($response), "<title>digiprove service temporarily offline</title>") != false)
+			{
+				return "The Digiprove service is temporarily offline for maintenance, please try again in a few minutes";
+			}
+		}
+		return $response;
+	}
+	catch (Exception $e)
+	{
+		$log->lwrite("Exception : " . $e->getMessage());
+		return 'Error: ' . $e->getMessage();
+	}
+}
+?>
---
+++ trunk/frustrate_copy.js
@@ -0,0 +1,196 @@
+//<![CDATA[ 
+// FUNCTIONS
+
+function disableSelection(target)
+{
+/*	// MozUserSelect has a bug - Bug 561691 : sub-elements cannot be excepted from the rule
+	if (typeof target.style.MozUserSelect!='undefined')    // Firefox
+	{
+		target.style.MozUserSelect='none';
+		enableInputElements(target);
+	}
+*/
+	if (typeof target.style.WebkitUserSelect != 'undefined')    // Safari or Chrome
+	{
+		target.style.WebkitUserSelect='none';
+		enableInputElements(target);
+	}
+	else
+	{
+		if (typeof target.onselectstart != 'undefined') //For IE, Chrome or Safari (but Chrome or Safari already picked up above)
+		{
+			target.onselectstart=function()
+			{
+				if (event.srcElement.type != "text" && event.srcElement.type != "textarea" && event.srcElement.type != "password")  // what about select and checkbox - seem to work ok having tested
+				{return false;}
+				else
+				{return true;}
+			}
+		}
+		else
+		{
+			if (window.sidebar)	// Firefox and Opera [and Netscape?], also IE and Chrome and Safari , but IE, Chrome and Safari already picked up above
+			{
+				target.onmousedown=trapMouseDown;
+			}
+			else //All other route (For Opera)
+			{
+				target.onmousedown=trapMouseDown;
+			}
+		}
+	}
+}
+
+function enableInputElements(target)
+{
+	var inputElements = target.getElementsByTagName("input");
+	for (var i=0; i<inputElements.length; i++)
+	{
+		enableSelection(inputElements[i]);
+	}
+	inputElements = target.getElementsByTagName("textarea");
+	for (var i=0; i<inputElements.length; i++)
+	{
+		enableSelection(inputElements[i]);
+	}
+}
+
+function enableSelection(target)
+{
+	if (typeof target.style.WebkitUserSelect!='undefined')    // Safari
+	{
+		target.style.WebkitUserSelect='text';
+	}
+}
+
+function trapMouseDown(e)
+{
+	var element
+	if (!e) var e = window.event
+	if (e.target) element = e.target
+	else if (e.srcElement) element = e.srcElement
+	if (element.nodeType == 3) // defeat Safari bug
+	element = element.parentNode
+	var tagname=element.tagName.toUpperCase();
+	if (tagname == "INPUT" || tagname == "TEXTAREA")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+
+// FUNCTIONS TO PREVENT RIGHT-CLICK:
+
+function disableRightClick()
+{
+	if (document.layers)
+	{
+		document.captureEvents(Event.MOUSEDOWN);
+		document.onmousedown=clickNS;
+	}
+	else
+	{
+		document.onmouseup=clickNS;
+	}
+	document.oncontextmenu=new Function("displayMessage();return false")
+}
+
+function clickNS(e)
+{
+	if	(document.layers||(document.getElementById&&!document.all))
+	{
+		if (e.which==2||e.which==3)	// Was it a right-click? 
+		{
+			displayMessage();
+			return false;
+		}
+	}
+	return true;
+}
+
+function displayMessage()
+{
+	if (justDisplayed == 0)
+	{
+		if (noRightClickMessage != "")
+		{
+			alert(htmlspecialchars_decode(noRightClickMessage));
+			justDisplayed = 1;
+			setTimeout("justDisplayed = 0;", 50);
+		}
+	}
+}
+
+function htmlspecialchars_decode(encodedString)
+{
+	var decodedString = encodedString.replace('&amp;', '&');
+	return decodedString.replace('&quot;', '"').replace('&#039;', '\'').replace('&lt;', '<').replace('&gt;','>');
+}
+
+
+
+// FUNCTIONS TO DISABLE CERTAIN CTRL KEY COMBINATIONS:
+function disableCtrlKeys()
+{
+	//alert("disableCtrlKeys starts");
+	// onkeypress is not triggered in safari with a ctrl/key combination
+	// onkeypress is not triggered in IE or Chrome with a function-linked ctrl/key combination (e.g ctrl/a, ctrl/c)
+	// onkeypress in IE gives different keyCode values (seems to be low numbers for ctrl/key and +32 for others) than onkeydown
+	// Set code to suppress ctrl/a and ctrl/u
+	//alert(navigator.userAgent);
+	if ((navigator.userAgent.indexOf('Safari') != -1) || navigator.userAgent.indexOf('MSIE') != -1)
+	{
+		document.onkeydown=trapCtrlKeyCombination;	// IE or Safari or Chrome
+	}
+	else
+	{
+	//alert("onkeypress");
+		document.onkeypress=trapCtrlKeyCombination;
+	}
+}
+
+function trapCtrlKeyCombination(ev)
+{
+        var key;
+        var isCtrl;
+		//ev=ev||event;
+		// TODO - change this test to checking whether undefined or not to avoid javascript warning message
+        if(window.event)	// This is true in IE and Safari  - Note ev.which also exists in Safari, Opera and Chrome
+        {
+			key = window.event.keyCode;
+			if (key == 17)    // this bit can be removed after testing
+			{
+				return true;
+			}
+			if(window.event.ctrlKey)
+					isCtrl = true;
+			else
+					isCtrl = false;
+
+		}
+        else
+        {
+			key = ev.which;     //firefox
+			if(ev.ctrlKey)
+					isCtrl = true;
+			else
+					isCtrl = false;
+        }
+
+        if(isCtrl)
+        {
+				if (String.fromCharCode(key).toLowerCase() == 'a' || String.fromCharCode(key).toLowerCase() == 'u')
+				{
+					void(0);  // CANCEL LAST EVENT
+					return false;
+				}
+		}
+        return true;
+}
+
+//]]>
